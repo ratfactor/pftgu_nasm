d9338784d421a8e68e622454276b3eadb69a2448
Dave Gauer <dave@ratfactor.com>
2022-10-05 17:04:15 -0400

    added ch03.2

---
+++ ch03.2_maximum.asm
@@ -0,0 +1,35 @@
+; Chapter 3 "Maximum"
+; 
+; Finds the largest value in a series of 4-byte values.
+; Demonstrates x86 memory access via: address + index * multiplier.
+;
+;    $ ./go.sh ch03.2_maximum
+;    Exited with code: 222
+;
+
+section .data
+
+; "double" sized items (32 bits) with zero sentinel at the end
+data_items: dd 3, 67, 34, 222, 45, 75, 54, 0
+
+section .text
+
+global _start
+
+_start:
+    mov edi, 0 ; start our data "index" at 0
+    mov ebx, 0 ; ebx will hold the largest value found
+
+start_loop:
+    mov eax, [data_items + edi * 4] ; get item by index (* 4 bytes)
+    cmp eax, 0       ; have we hit the sentinel?
+    je loop_exit     ; ...yes, we're done with the list
+    inc edi          ; move index to next value to be checked.
+    cmp eax, ebx     ; Is this value bigger?
+    jle start_loop   ; ...no. Try the next value.
+    mov ebx, eax     ; ...yes. Store it...
+    jmp start_loop   ; ...and check next value
+
+loop_exit:     ; ebx contains largest value, so that's our exit status
+    mov eax, 1 ; linux syscall 'exit'
+    int 0x80   ; interrupt to call linux kernel
--- go.sh
+++ go.sh
@@ -4,7 +4,8 @@ set -e # exit this script upon error
 
 mkdir -p bin_out
 
-nasm -f elf32 -o $1.o $1.asm 
+# -g adds debugging info
+nasm -f elf32 -g -o $1.o $1.asm 
 ld -m elf_i386 $1.o -o bin_out/$1
 rm $1.o
 
